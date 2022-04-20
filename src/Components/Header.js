f803fbd6637aaa0879d33dc6b31881d4c821e91c
Onke <sipukaom@gmail.com>
2022-04-21 00:48:59 +0200

    Updated Header information

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,55 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var city= this.props.data.address.city;
-      var quote= this.props.data.quote;
-      var current_occupation= this.props.data.current_occupation;
-      var next_occupation= this.props.data.next_occupation;
-      var goal= this.props.data.goal;
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
+      var city = this.props.data.address.city;
+      var quote = this.props.data.quote;
+      var current_occupation = this.props.data.current_occupation;
+      var employer = this.props.data.employer;
+      var goal = this.props.data.goal;
+      var networks = this.props.data.social.map(function (network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
     }
 
     return (
       <header id="home">
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
 
-      <nav id="nav-wrap">
-
-         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Quotes</a></li>
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#testimonials">
+                Quotes
+              </a>
+            </li>
             {/* <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
-         </ul>
+          </ul>
+        </nav>
 
-      </nav>
-
-      <div className="row banner">
-         <div className="banner-text">
+        <div className="row banner">
+          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}</h1>
-            <h3>I'm a <span>{current_occupation}</span> based in {city}. I am a <span>{next_occupation}</span> excited about {goal}.<br/><span>"{quote}"</span></h3>
+            <h3>
+              I'm a <span>{current_occupation}</span> based in {city}. I am
+              currently employed by <span>{employer}</span> where I{" "}
+              <span>{goal}</span>.
+              <br />
+              <span>"{quote}"</span>
+            </h3>
             <hr />
-            <ul className="social">
-               {networks}
-            </ul>
-         </div>
-      </div>
+            <ul className="social">{networks}</ul>
+          </div>
+        </div>
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
     );
   }
 }
